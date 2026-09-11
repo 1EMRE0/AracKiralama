9bd1cb7658d75e8d3896dd29880a6d75f40816aa
1EMRE0 <ovacike7@gmail.com>
2026-09-11 15:23:15 +0300

    Add project files.

---
+++ Core/Arac.Domain/Entities/Banner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arac.Domain.Entities
+{
+    public class Banner
+    {
+        public int BannerID { get; set; }
+        public int Title { get; set; }
+        public string BannerDesc { get; set; }
+        public string BannerVideoDesc { get; set; }
+        public string BannerUrl { get; set; }
+      
+    }
+}
