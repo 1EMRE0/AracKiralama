e5035b6b6031467a8791a8972429ab7ff943340a
1EMRE0 <ovacike7@gmail.com>
2026-09-11 15:47:10 +0300

    entities sınıflar eklendi

---
+++ Core/Arac.Domain/Entities/Brand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arac.Domain.Entities
+{
+    public class Brand
+    {
+        public int BrandID { get; set; }
+        public string BrandName { get; set; }
+        public List<Car> Cars { get; set; }
+    }
+}
---
+++ Core/Arac.Domain/Entities/Location.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arac.Domain.Entities
+{
+    public class Location
+    {
+        public int LocationID { get; set; }
+        public string LocationName { get; set; }
+
+    }
+}
